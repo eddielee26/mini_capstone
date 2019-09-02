c85ff09bb653bdedf73fc475df87a2629e3135a4
eddie <eddielee001@gmail.com>
2019-09-02 12:57:30 -0500

    adds all REST methods

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -1,12 +1,57 @@
 class Api::ProductsController < ApplicationController
 
-  def product_all
-    @products = Product.all
-    render 'product_all.json.jb'
+  # def product_all
+  #   @products = Product.all
+  #   render 'product_all.json.jb'
+  # end
+
+  # def product_individual
+  #   @product = Product.all[0]
+  #   render 'product_individual.json.jb'
+  # end
+
+  # def product_query
+  #   product_id = params[:id]
+  #   @product = Product.find_by(id: product_id)
+  #   render 'product.json.jb'
+  # end
+
+  def index
+    @products_index = Product.all
+    render 'product_index.json.jb'
   end
 
-  def product_individual
-    @product = Product.all[0]
-    render 'product_individual.json.jb'
+  def show
+    @product = Product.find_by(id: params[:id])
+    render 'product_show.json.jb'
+  end
+
+  def create
+    @product = Product.create(
+      name: params[:name],
+      price: params[:price].to_i,
+      image_url: params[:image_url],
+      description: params[:description]
+    )
+    @product.save
+    render 'product_show.json.jb'
+  end
+
+  def update
+    @product = Product.find_by(id: params[:id])
+    @product.id = params[:id] || @product.id
+    @product.name = params[:name] || @product.name
+    @product.price = params[:price] || @product.price
+    @product.image_url = params[:image_url] || @product.image_url
+    @product.description = params[:description] || @product.description
+    
+    @product.save
+    render 'product_show.json.jb'
+  end
+
+  def destroy
+    product = Product.find_by(id: params[:id])
+    product.destroy
+    render json: {message: "Recipe destroyed!"}
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,22 @@ Rails.application.routes.draw do
   #   get "/photos" => "photos#index"
   # end
   namespace :api do
-    get "/products_all" => "products#product_all"
-    get "/products_individual" => "products#product_individual"
+    # get "/products_all" => "products#product_all"
+
+    # get "/products_individual" => "products#product_individual"
+
+    # get "/product" => "products#product_query"
+
+    # get "/product/:id" => "products#product_query"
+
+    get "/products" => "products#index"
+
+    get "/products/:id" => "products#show"
+
+    post "/products" => "products#create"
+
+    patch "products/:id" => "products#update"
+
+    delete "products/:id" => "products#destroy"
   end
 end
