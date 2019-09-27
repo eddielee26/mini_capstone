428c465195e982a76e6a98f020a2d8bfca257b26
eddie <eddielee001@gmail.com>
2019-09-27 11:47:11 -0500

    adds seed dump

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,34 +1,45 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+User.create!([
+  {name: "eddie", email: "eddie@gmail.com", password_digest: "$2a$12$pvftiIbuf7Tbv/f2ISGkY.W4O7G6Gtj63vch5vrzdb2mSBbw29IWK", admin: true},
+  {name: "jay", email: "jay@gmail.com", password_digest: "$2a$12$MMfnQhoS8sc4rpxGsCG9AOa2hiwlWi5TGM8E/Xr3/3iUZ/8tRQ99q", admin: false}
+])
+Category.create!([
+  {name: "electronics"},
+  {name: "games"}
+])
+Image.create!([
+  {url: "https://ss7.vzw.com/is/image/VerizonWireless/palm-phone-gold?$device-lg$", product_id: 3}
+])
+Order.create!([
+  {user_id: 1, subtotal: "500.0", tax: "45.0", total: "545.0"},
+  {user_id: 1, subtotal: "1800.0", tax: "162.0", total: "1962.0"},
+  {user_id: 1, subtotal: "150.0", tax: "13.5", total: "163.5"},
+  {user_id: 1, subtotal: "3400.0", tax: "306.0", total: "3706.0"}
+])
+Supplier.create!([
+  {name: "Amazon", email: "amazon@gmail.com", phone_number: "121-1212"},
+  {name: "Newegg", email: "newegg@gmail.com", phone_number: "232-2323"},
+  {name: "Thinkgeek", email: "thinkgeek@gmail.com", phone_number: "343-3434"}
+])
+Product.create!([
+  {name: "laptop", price: "900.0", description: "alienware", supplier_id: 3},
+  {name: "super smash bros ultimate", price: "50.0", description: "nintendo", supplier_id: 3},
+  {name: "diablo 2 lord of destruction", price: "10.0", description: "best pc game ever made by blizzard. none like this exists. ", supplier_id: 2},
+  {name: "phone", price: "500.0", description: "black", supplier_id: 3}
+])
+ProductCategory.create!([
+  {product_id: 1, category_id: 1},
+  {product_id: 2, category_id: 2},
+  {product_id: 3, category_id: 1}
+])
+CartedProduct.create!([
+  {user_id: 1, product_id: 1, quantity: nil, status: nil, order_id: 1},
+  {user_id: 1, product_id: 2, quantity: nil, status: nil, order_id: 2},
+  {user_id: 1, product_id: 3, quantity: nil, status: nil, order_id: 3},
+  {user_id: 2, product_id: 1, quantity: nil, status: nil, order_id: 4},
+  {user_id: 2, product_id: 2, quantity: nil, status: nil, order_id: 5},
+  {user_id: 1, product_id: 1, quantity: 2, status: "purchased", order_id: 26},
+  {user_id: 1, product_id: 2, quantity: 2, status: "purchased", order_id: 26},
+  {user_id: 1, product_id: 3, quantity: 3, status: "purchased", order_id: 26},
+  {user_id: 2, product_id: 3, quantity: nil, status: "removed", order_id: 6}
+])
 
-# product = Product.new(
-#   name: "name",
-#   price: 1,
-#   image_url: "image_url",
-#   description: "description"
-# )
-
-# product.save
-
-# product = Product.new(
-#   name: "name2",
-#   price: 2,
-#   image_url: "image_url2",
-#   description: "description2"
-# )
-
-# product.save
-
-# Supplier.create(name: "Amazon", email: "amazon@gmail.com", phone_number: "121-1212")
-# Supplier.create(name: "Newegg", email: "newegg@gmail.com", phone_number: "232-2323")
-# Supplier.create(name: "Thinkgeek", email: "thinkgeek@gmail.com", phone_number: "343-3434")
-
-# supplier is farmer, product is cow
-# image is cow, product is farmer
-
-# Image.create(url: "https://ss7.vzw.com/is/image/VerizonWireless/palm-phone-gold?$device-lg$", product_id: 3)
