03144dc0c191ce97848622d07932a01a38e3ae8a
eddie <eddielee001@gmail.com>
2019-09-27 11:50:43 -0500

    makes changes to seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,6 +2,17 @@ User.create!([
   {name: "eddie", email: "eddie@gmail.com", password_digest: "$2a$12$pvftiIbuf7Tbv/f2ISGkY.W4O7G6Gtj63vch5vrzdb2mSBbw29IWK", admin: true},
   {name: "jay", email: "jay@gmail.com", password_digest: "$2a$12$MMfnQhoS8sc4rpxGsCG9AOa2hiwlWi5TGM8E/Xr3/3iUZ/8tRQ99q", admin: false}
 ])
+Supplier.create!([
+  {name: "Amazon", email: "amazon@gmail.com", phone_number: "121-1212"},
+  {name: "Newegg", email: "newegg@gmail.com", phone_number: "232-2323"},
+  {name: "Thinkgeek", email: "thinkgeek@gmail.com", phone_number: "343-3434"}
+])
+Product.create!([
+  {name: "laptop", price: "900.0", description: "alienware", supplier_id: 3},
+  {name: "super smash bros ultimate", price: "50.0", description: "nintendo", supplier_id: 3},
+  {name: "diablo 2 lord of destruction", price: "10.0", description: "best pc game ever made by blizzard. none like this exists. ", supplier_id: 2},
+  {name: "phone", price: "500.0", description: "black", supplier_id: 3}
+])
 Category.create!([
   {name: "electronics"},
   {name: "games"}
@@ -14,17 +25,6 @@ Order.create!([
   {user_id: 1, subtotal: "1800.0", tax: "162.0", total: "1962.0"},
   {user_id: 1, subtotal: "150.0", tax: "13.5", total: "163.5"},
   {user_id: 1, subtotal: "3400.0", tax: "306.0", total: "3706.0"}
-])
-Supplier.create!([
-  {name: "Amazon", email: "amazon@gmail.com", phone_number: "121-1212"},
-  {name: "Newegg", email: "newegg@gmail.com", phone_number: "232-2323"},
-  {name: "Thinkgeek", email: "thinkgeek@gmail.com", phone_number: "343-3434"}
-])
-Product.create!([
-  {name: "laptop", price: "900.0", description: "alienware", supplier_id: 3},
-  {name: "super smash bros ultimate", price: "50.0", description: "nintendo", supplier_id: 3},
-  {name: "diablo 2 lord of destruction", price: "10.0", description: "best pc game ever made by blizzard. none like this exists. ", supplier_id: 2},
-  {name: "phone", price: "500.0", description: "black", supplier_id: 3}
 ])
 ProductCategory.create!([
   {product_id: 1, category_id: 1},
